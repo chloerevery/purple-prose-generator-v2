daccb828eb688763281a7c20f3ad4a94e0b88bcd
chloerevery <chloerevery@gmail.com>
2016-08-03 00:43:34 -0400

    Implemented remainder of core functionality

--- app.py
+++ app.py
@@ -12,31 +12,54 @@ def home():
     	to_translate = request.form['prose']
 
     	word_list = to_translate.split(" ") # Split toTranslate on spaces
-    	purplified_prose = "" # Create purplifiedProse string
         
         # Create map with key [original word in toTranslate], value [lowercase, non-punctuated equivalent]
-        word_map = {}
+        word_map = {} # TODO(Chloe): Handle case where sentence contains same word twice, in different contexts
         for word in word_list:
         	word_map[word] = helpers.strip(word)
         
         for word in word_map.values(): # For each value in map
-        		# StringToAppendToPurplifiedProse <-- value
-        		# Result = result of call to thesaurus api
-	        		# For now, assume all words are nouns if thesaurus api returns an array of noun synonyms
-	        		# Later: integrate sentence parsing API to determine likely part of speech of each word
-        		# If result.nouns != null
-        			# Iterate through result, return longest synonym
-        			# (Stretch) use map to mimic original punctuation and case
-        			# StringToAppendToPurplifiedProse --> longest synonym
-        		# Append StringToAppendToPurplifiedProse to purplifiedProse
+        		curr_word = word # StringToAppendToPurplifiedProse <-- value
+        		# Resp = result of call to thesaurus api
+        		url = helpers.build_query(word) 
+        		print(url)
+        		try: 
+        			resp = requests.get(url) # TODO(Chloe): Add error handling
+        		except requests.exceptions.RequestException as e:    # This is the correct syntax
+        			print e
+        		if resp.status_code != 200:
+        			word_map[word] = word
+        			break
+				    
+	        	# For now, assume all words are nouns if thesaurus api returns an array of noun synonyms
+	        	# Later: integrate sentence parsing API to determine likely part of speech of each word
+	        	longest_syn = ""
+	        	if 'noun' in resp.json().keys():
+	        		if 'syn' in resp.json()['noun'].keys():
+	        			longest_syn = helpers.get_longest_syn(resp.json()['noun']['syn'])
+
+        		longest_syn = longest_syn if len(longest_syn) > len(word) else word
+        		print("longest syn: " + longest_syn)
+
+        		# add longest syn to map
+        		word_map[word] = longest_syn
+
+        		# TODO(Chloe): Use map to mimic original punctuation and case
+
+        purplified_prose = ""
+        print(word_map)
+        for word in word_list:
+        	purplified_prose += word_map[word] + " "
+
+
+        		
         # Send purplifiedProse into 'results' div
 
-        url = "http://words.bighugelabs.com/api/2/0b4bb7369d73af737d44f66e89f66c99/" + to_translate + "/json"
-        
-        response = requests.get(url)
-        results = jsonify(response.json())
 
-       	return render_template('index.html', results=results)
+        
+        
+
+       	return render_template('index.html', results=purplified_prose)
 
     return render_template('index.html')
 
--- helpers.py
+++ helpers.py
@@ -1,8 +1,25 @@
 import re
 
 def strip(word):
-	regex = re.compile('[^a-zA-Z]')
-	regex.sub('', word)
-	word = word.lower()
+	print("converted " + word)
 
+	regex = re.compile('[^a-zA-Z]+') 
+	regex.sub('', word) # TODO(Chloe): This code doesn't work
+	word = word.lower()
+	print(" to " + word)
 	return word
+
+def build_query(word):
+	base_url = "http://words.bighugelabs.com/api"
+	version = "2"
+	api_key = "wouldn't you like to know" # TODO(Chloe): Factor this out into config
+	data_format = "json"
+	url = '{base_url}/{version}/{api_key}/{word}/{data_format}'.format(base_url=base_url, version=version, api_key=api_key, word=word, data_format=data_format)
+	return url
+
+def get_longest_syn(syn_list):
+	longest_syn = ""
+	for syn in syn_list: # For each value in map
+		if len(syn) > len(longest_syn):
+			longest_syn = syn
+	return longest_syn
